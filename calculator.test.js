0788b8696d9a8224352d5aff47beb57fbae9aafd
brittanymbui <brittymbui@gmail.com>
2021-09-06 21:39:22 -0700

    Removes server file

--- calculator.test.js
+++ calculator.test.js
@@ -32,4 +32,18 @@ describe("Calculator tests", () => {
         // pass - null treated as false or negative
         expect(n).toBeFalsy()
     })
+    test('numeric operators', () => {
+        var num1 = 100;
+        var num2 = -20;
+        var num3 = 0;
+
+        // greater than
+        expect(num1).toBeGreaterThan(10)
+
+        // less than or equal to
+        expect(num2).toBeLessThanOrEqual(0)
+
+        // greater than or equal to
+        expect(num3).toBeGreaterThanOrEqual(0)
+    })
 });
